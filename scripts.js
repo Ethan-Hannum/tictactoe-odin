56954c2af61485dbe2b57c99d04eac64e3bb791f
Ethan Hannum <ethanhannum10@gmail.com>
2026-04-01 13:39:35 -0700

    Make tie games seperate from winning/placing
    When a game finished tied, meaning no spots left to place markers and
    no one won, nothing would happen. The game still expects the next player
    to place a marker.
    
    To fix this an extra else if condition was added to checkWinner.
    The statement checks if there are no nulls in the boardArr. If true then
    set gameStatus to 2.
    Function changeStatus will now just change the status to any number
    given in it's parameters, but that will always be 0, 1 or 2. 1 means
    someone has won the game and 2 means the game was tied. Now also in
    playRound and extra condition was set to check if checkStatus was 2, so
    that the game wouldn't just say next players turn.

--- scripts.js
+++ scripts.js
@@ -11,9 +11,6 @@ function createUser(xOrO, player) {
     return {getScore, addScore, getMarker, getName};
 };
 
-const player1 = createUser("X", "Nichsolson");
-const player2 = createUser("O", "Travis");
-
 const gameBoard = (() => {
     const boardArr = [null, null, null, null, null, null, null, null, null];
     console.log(boardArr);
@@ -26,30 +23,31 @@ const gameBoard = (() => {
     const checkWinner = (marker) => {
         if ((boardArr[0] == marker) && (boardArr[1] == marker) && (boardArr[2] == marker)) {
             console.log("Someone won at 0, 1 and 2");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[3] == marker) && (boardArr[4] == marker) && (boardArr[5] == marker)) {
             console.log("Someone won at 3, 4 and 5");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[6] == marker) && (boardArr[7] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 6, 7 and 8");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[0] == marker) && (boardArr[3] == marker) && (boardArr[6] == marker)) {
             console.log("Someone won at 0, 3 and 6");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[1] == marker) && (boardArr[4] == marker) && (boardArr[7] == marker)) {
             console.log("Someone won at 1, 4 and 7");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[2] == marker) && (boardArr[5] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 2, 5 and 8");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[0] == marker) && (boardArr[4] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 0, 4 and 8");
-            gameFlow.changeStatus();
+            gameFlow.changeStatus(1);
         } else if ((boardArr[2] == marker) && (boardArr[4] == marker) && (boardArr[6] == marker)) {
             console.log("Someone won at 2, 4 and 6");
-            gameFlow.changeStatus();
-        } else {
-            console.log("no one won");
+            gameFlow.changeStatus(1);
+        } else if (!boardArr.includes(null)) {
+            console.log("checkWinner: Tie game!");
+            gameFlow.changeStatus(2);
         }
     }
     
@@ -57,6 +55,8 @@ const gameBoard = (() => {
 })();
 
 const gameFlow = (() => {
+    const player1 = createUser("X", "Nichsolson");
+    const player2 = createUser("O", "Travis");
     console.log("Start of game, player 1's turn");
     let gameStatus = 0;
     let currentPlayer = player1;
@@ -67,8 +67,8 @@ const gameFlow = (() => {
          : currentPlayer = player1;
     }
 
-    const changeStatus = () => {
-        ++gameStatus;
+    const changeStatus = (score) => {
+        gameStatus = score;
         console.log(`Game status: ${gameStatus}`);
     }
 
@@ -79,6 +79,8 @@ const gameFlow = (() => {
         gameBoard.checkWinner(currentPlayer.getMarker());
         if (gameStatus === 1) {
             console.log(`${currentPlayer.getName()} has won the game!`);
+        } else if (gameStatus === 2) {
+            console.log("playRound: Tie game!")
         } else {
             changePlayer();
             console.log(`It is now ${currentPlayer.getName()}'s turn`);
