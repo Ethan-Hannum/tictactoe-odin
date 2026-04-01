2d1cd4b5003412587d0661228b72ce0517b26bfc
Ethan Hannum <ethanhannum10@gmail.com>
2026-04-01 12:50:45 -0700

    Make the game flow properly between two players
    Would get undefined errors with checkStatus while trying to call
    functions in the gameBoard method. Embedding the number infront of
    "player" just would not give the expected result.
    
    Now currentPlayer directly refrences either player1 or player2. When
    changePlayer is called it will check if it is currently player1. If so,
    swap to player2, else swap to player1.
    
    Also added the name property and getName function to createUser.
    Now instead of logging "player1" or "player2" a name will be given to a
    player and this will be logged.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-function createUser(xOrO) {
+function createUser(xOrO, player) {
     const marker = xOrO;
+    const name = player;
     const score = 0;
 
     const getMarker = () => marker;
+    const getName = () => name;
     const getScore = () => score;
     const addScore = () => { score++ };
 
-    return {getScore, addScore, getMarker};
+    return {getScore, addScore, getMarker, getName};
 };
 
-const player1 = createUser("X");
-const player2 = createUser("O");
+const player1 = createUser("X", "Nichsolson");
+const player2 = createUser("O", "Travis");
 
 const gameBoard = (() => {
     const boardArr = [null, null, null, null, null, null, null, null, null];
     console.log(boardArr);
-    const {getMarker} = createUser();
 
     const placeMarker = (marker, item) => {
         boardArr[item] = `${marker}`;
@@ -25,28 +26,28 @@ const gameBoard = (() => {
     const checkWinner = (marker) => {
         if ((boardArr[0] == marker) && (boardArr[1] == marker) && (boardArr[2] == marker)) {
             console.log("Someone won at 0, 1 and 2");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[3] == marker) && (boardArr[4] == marker) && (boardArr[5] == marker)) {
             console.log("Someone won at 3, 4 and 5");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[6] == marker) && (boardArr[7] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 6, 7 and 8");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[0] == marker) && (boardArr[3] == marker) && (boardArr[6] == marker)) {
             console.log("Someone won at 0, 3 and 6");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[1] == marker) && (boardArr[4] == marker) && (boardArr[7] == marker)) {
             console.log("Someone won at 1, 4 and 7");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[2] == marker) && (boardArr[5] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 2, 5 and 8");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[0] == marker) && (boardArr[4] == marker) && (boardArr[8] == marker)) {
             console.log("Someone won at 0, 4 and 8");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else if ((boardArr[2] == marker) && (boardArr[4] == marker) && (boardArr[6] == marker)) {
             console.log("Someone won at 2, 4 and 6");
-            ++gameStatus;
+            gameFlow.changeStatus();
         } else {
             console.log("no one won");
         }
@@ -56,32 +57,33 @@ const gameBoard = (() => {
 })();
 
 const gameFlow = (() => {
-    const {getMarker, addScore} = createUser();
-    
     console.log("Start of game, player 1's turn");
-    const gameStatus = 0;
-    const currentPlayer = 1;
+    let gameStatus = 0;
+    let currentPlayer = player1;
     
     const changePlayer = () => {
-        currentPlayer === 1 ? ++currentPlayer : --currentPlayer;
+        currentPlayer === player1
+         ? currentPlayer = player2 
+         : currentPlayer = player1;
     }
 
-    const checkStatus = () => {
-        if (gameStatus === 1) {
-            console.log(`Player ${currentPlayer} has won the game!`);
-        } else {
-            changePlayer();
-            console.log(`It is now Player ${currentPlayer}'s turn`);
-        }
+    const changeStatus = () => {
+        ++gameStatus;
+        console.log(`Game status: ${gameStatus}`);
     }
 
     const playRound = (item) => {
         if (gameStatus === 1) return;
 
-        gameBoard.placeMarker(`player${currentPlayer}`.getMarker(), item);
-        gameBoard.checkWinner(`player${currentPlayer}`.getMarker());
-        checkStatus();
+        gameBoard.placeMarker(currentPlayer.getMarker(), item);
+        gameBoard.checkWinner(currentPlayer.getMarker());
+        if (gameStatus === 1) {
+            console.log(`${currentPlayer.getName()} has won the game!`);
+        } else {
+            changePlayer();
+            console.log(`It is now ${currentPlayer.getName()}'s turn`);
+        }
     }
 
-    return {playRound};
+    return {changeStatus, playRound};
 })();
